f3eeb9c3340c8752071d101e067eac7ba30271ef
Onelio Viera <ojvwebdesign@gmail.com>
2023-08-13 19:43:31 -0400

    updated files

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
+import {useState} from 'react';
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
+
 function App() {
-  const title = 'Blog Post';
-  const body = 'This is my blog post';
-  const comments = [
-  {id: 1, text: 'Comment one'},
-  {id: 2, text: 'Comment two'},
-  {id: 3, text: 'Comment three'},
-  ];
+  const [feedback, setFeedback] = useState (FeedbackData);
 
   return (
+    <>
+    <Header />
     <div className="container">
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-      <div className="comments">
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>
-              {comment.text}
-            </li>
-          ))}
-        </ul>
-      </div>
+      <FeedbackList feedback={feedback} />
     </div>
-
+    </>
   );
 }
+
 
 export default App;

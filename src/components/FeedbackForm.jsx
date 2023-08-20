a898f20e2656120b7dea19ead270df51d8f30dbc
Onelio Viera <ojvwebdesign@gmail.com>
2023-08-19 20:32:20 -0400

    new update

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,10 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import RatingSelect from './RatingSelect';
 import Card from './shared/Card';
 import Button from './shared/Button';
 
 
-function FeedbackForm() {
+function FeedbackForm({handleAdd}) {
   const [text, setText] = useState('');
   const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState('true');
@@ -12,10 +12,10 @@ function FeedbackForm() {
 
 
   const handleTextChange = (e) => {
-    if(text === '') {
+    if (text === '') {
       setBtnDisabled(true)
       setMessage(null)
-    } else if(text !== '' && text.trim().length <= 10) {
+    } else if (text !== '' && text.trim().length <= 10) {
       setMessage('Your review must be at least 10 characters.')
       setBtnDisabled(true)
     } else {
@@ -27,17 +27,31 @@ function FeedbackForm() {
     setText(e.target.value);
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(text.trim().length > 10) { 
+      const newFeedback = {
+        text,
+        rating,
+    }
+
+    handleAdd(newFeedback)
+
+    setText('')
+  }
+}
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
-        <RatingSelect />
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
-          <input 
-          onChange={handleTextChange} 
-          type="text" 
-          placeholder='Write a review'
-          value={text} />
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder='Write a review'
+            value={text} />
           <Button type="submit" isDisabled={btnDisabled}>Send</Button>
         </div>
 
